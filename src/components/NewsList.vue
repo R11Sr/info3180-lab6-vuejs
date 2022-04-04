a8b43f147d03dbfa0dec2e4233bfc89966d641a2
r11Sr <rs_burner2@outlook.com>
2022-04-04 14:05:30 -0500

    added search box to fetch requested content

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,50 +1,69 @@
 <template>
-    <ul class="news__list">
+    <form action="" @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+        <div class="input-group mx-sm-3 mb-2">
+            <label class="visually-hidden" for="search">Search</label>
+            <input type="search" name="search" v-model="searchTerm" id="search" 
+            class="form-control mb-2 mr-sm-2" placeholder="Enter Search Term Here">
+            <button class="btn btn-primary mb-2">Search</button>
+        </div>
+        <p>You are searching for{{searchTerm}}</p>
+    </form>
 
-<!-- 
-        <li v-for="(article,index) in articles" :key=index  class="news__item">
-            {{article.title}}
-        </li> -->
-
-        <div class="container">
-            <div class="row">
-                <div class="col-3 pb-4 news__item" v-for="(article,index) in articles" :key=index>
-                    <div class="card">
-                        <img class="card-img-top" :src="article.urlToImage" :alt="article.title" srcset="">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                {{article.title}}
-                            </h5>
-                            <p class="pb-1 text muted">
-                                {{article.content}}
-                            </p>
-                        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 pb-4 news__item" v-for="(article,index) in articles" :key=index>
+                <div class="card">
+                    <img class="card-img-top" :src="article.urlToImage" :alt="article.title" srcset="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            {{article.title}}
+                        </h5>
+                        <p class="pb-1 text muted">
+                            {{article.content}}
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
-    </ul>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
       return {
-          articles: []
+          articles: [],
+          searchTerm:''
       }        
     },
-    created(){
-        fetch('https://newsapi.org/v2/top-headlines?country=us',{
-            headers:{
-                'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
-            }
-        }).then((response)=>{
-            return response.json();
-        }).then((data)=>{
-            console.log(data);
-            this.articles = data.articles;
-            })
+    methods:{
+        searchNews(){
+            fetch('https://newsapi.org/v2/everything?q='+
+                this.searchTerm + '&language=en', {
+                    headers: {
+                        'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
+                    }
+                }).then((response)=>{
+                    return response.json();
+                }).then((data)=>{
+                    // console.log(data);
+                    this.articles = data.articles;
+                })
+        }
     }
+    //  created(){
+    //     fetch('https://newsapi.org/v2/top-headlines?country=us',{
+    //         headers:{
+    //             'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+    //         }
+    //     }).then((response)=>{
+    //         return response.json();
+    //     }).then((data)=>{
+    //         console.log(data);
+    //         this.articles = data.articles;
+    //         })
+    // }
+
 }
 </script>
 
