21b81c79b5cc28aa44075d83c66fc41fe57d52b2
r11Sr <rs_burner2@outlook.com>
2022-04-04 13:24:32 -0500

    added fetched content from API to UI

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,8 +1,28 @@
 <template>
     <ul class="news__list">
-        <li class="news__item">News Item 1</li>
-        <li class="news__item">News Item 2</li>
-        <li class="news__item">News Item 3</li>
+
+<!-- 
+        <li v-for="(article,index) in articles" :key=index  class="news__item">
+            {{article.title}}
+        </li> -->
+
+        <div class="container">
+            <div class="row">
+                <div class="col-3 pb-4 news__item" v-for="(article,index) in articles" :key=index>
+                    <div class="card">
+                        <img class="card-img-top" :src="article.urlToImage" :alt="article.title" srcset="">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                {{article.title}}
+                            </h5>
+                            <p class="pb-1 text muted">
+                                {{article.content}}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </ul>
 </template>
 
@@ -10,7 +30,20 @@
 export default {
     data() {
       return {
+          articles: []
       }        
+    },
+    created(){
+        fetch('https://newsapi.org/v2/top-headlines?country=us',{
+            headers:{
+                'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+            }
+        }).then((response)=>{
+            return response.json();
+        }).then((data)=>{
+            console.log(data);
+            this.articles = data.articles;
+            })
     }
 }
 </script>
